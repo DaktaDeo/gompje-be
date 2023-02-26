a30569d7adc69ea3c7ca1b83b0be4324ee18be6c
Veerle Deschepper <veerle@daktadeo.be>
2023-02-26 02:00:34 +0100

    fix: should fix weird link issue on server

--- resources/views/components/post-preview.blade.php
+++ resources/views/components/post-preview.blade.php
@@ -1,6 +1,5 @@
 <div class="mt-2 spaced-y-10 mb-10">
     <div>
-
         <div class="flex mt-1 text-gray-500 text-xs mb-2 gap-2">
             <x-relative-time :date-time="$post->release_date" prefix="Published →"/>
             @if(!$post->is_compact)
@@ -18,16 +17,8 @@
                 {!! $post->content !!}
             </div>
         @else
-            <a href="{{$post->view}}"
-               class="
-        text-gray-900
-        dark:text-gray-200
-        text-xl
-        font-bold
-        no-underline
-        hover:underline hover:text-indigo-400
-        dark:hover:text-indigo-400
-      ">
+            <a href="/{{$post->view}}"
+               class=" text-gray-900 dark:text-gray-200 text-xl font-bold no-underline hover:underline hover:text-indigo-400 dark:hover:text-indigo-400 ">
                 {{ $post->title }}
             </a>
         @endif
@@ -41,7 +32,7 @@
 
     @if(!$post->is_compact)
         <div class="text-base leading-normal mt-2">
-            <a href="{{$post->view}}"
+            <a href="/{{$post->view}}"
                class=" text-gray-900 dark:text-gray-200 hover:text-indigo-400 dark:hover:text-indigo-400 text-sm no-underline hover:underline "
                style="outline-width: 0px !important; user-select: auto !important;"
             >
